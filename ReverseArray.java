4fba7cab87d0569815baf336a9463d9c9b71de82
Ravi Agrawal <agrawalravi636@gmail.com>
2025-08-03 21:03:21 +0530

    Add Reverse Array Code

---
+++ ReverseArray.java
@@ -0,0 +1,26 @@
+public class ReverseArray {
+    public static void reverse(int arr[]) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            // swap
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        reverse(arr);
+        print(arr);
+    }
+}
